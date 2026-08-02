d697b56a080421fe99c3910b74b8b7732c81891b
SnrGottes <gottesprikhnenko@gmail.com>
2026-08-02 16:22:10 +0300

    Add the ability to change the coordinate axes of graphs via the interface

--- src/gui/widgets/base_components.py
+++ src/gui/widgets/base_components.py
@@ -1,12 +1,14 @@
 import pyqtgraph as pg
 import numpy as np
+from PyQt6.QtCore import pyqtSignal, Qt
 from PyQt6.QtWidgets import (
     QMainWindow, QVBoxLayout, QHBoxLayout, QGridLayout,
     QLabel, QWidget, QPushButton, QDoubleSpinBox
 )
 from src.config_loader import ConfigLoader
 
-settings = ConfigLoader.get_gui_settings()
+gui_settings = ConfigLoader.get_gui_settings()
+sim_settings = ConfigLoader.get_sim_settings()
 
 class DataSpinBox(QDoubleSpinBox):
     def __init__(self, min_v: float, max_v: float, default: float, step: float,  parent=None):
@@ -15,14 +17,21 @@ class DataSpinBox(QDoubleSpinBox):
         self.setRange(min_v, max_v)
         self.setValue(default)
         self.setSingleStep(step)
-        self.setDecimals(settings['spin']['decimals'])
+        self.setDecimals(gui_settings['spin']['decimals'])
 
 class BaseButton(QPushButton):
     def __init__(self, text: str,  parent=None):
         super().__init__(parent)
 
         self.setText(text)
-        self.setObjectName('standart_button')
+        self.setObjectName('base_button')
+
+class AxisButton(QPushButton):
+    def __init__(self, text: str, parent=None):
+        super().__init__(parent)
+
+        self.setText(text)
+        self.setObjectName('axis_button')
 
 class ColorDisplay(QWidget):
     def __init__(self, color: str, axis_number: int, parent=None):
@@ -51,8 +60,61 @@ class GraphWidget(pg.PlotWidget):
         self.showGrid(x=True, y=True)
         self.setTitle('')
         self.setFixedSize(widget_size, widget_size)
-        self.setLabel('left', f'Axis {self.y_idx}')
-        self.setLabel('bottom', f'Axis {self.x_idx}')
+        self.setLabel('left', f'Axis X<sub>{self.y_idx}</sub>')
+        self.setLabel('bottom', f'Axis X<sub>{self.x_idx}</sub>')
 
     def _on_points_clicked(self):
         pass
+
+    def edit_axis(self, changing_axis: str, axis: int):
+        self.setLabel(changing_axis, f'Axis X<sub>{axis}</sub>')
+
+    def edit_left_axis(self, axis: int):
+        self.edit_axis('left', axis)
+
+    def edit_bottom_axis(self, axis: int):
+        self.edit_axis('bottom', axis)
+
+class AxisSelectionWidget(QWidget):
+    edit_axis_signal = pyqtSignal(int)
+
+    def __init__(self, axis: int, widget_size: int, parent=None):
+        super().__init__(parent)
+
+        self.layout = QHBoxLayout()
+        self.layout.setContentsMargins(5, 0, 5, 0)
+        self.setFixedHeight(int(widget_size/2))
+
+        self.axis = axis
+
+        self.left_button = AxisButton('←')
+        self.label = QLabel(f'X<sub>{self.axis}</sub')
+        self.right_button = AxisButton('→')
+
+        self.label.setObjectName('axis_label')
+
+        self.left_button.clicked.connect(self.left_clicked)
+        self.right_button.clicked.connect(self.right_clicked)
+
+        for widget in [self.left_button, self.label, self.right_button]:
+            self.layout.addWidget(widget, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        self.setLayout(self.layout)
+
+    def left_clicked(self):
+        self.axis -= 1
+        if self.axis <= 0:
+            self.axis = sim_settings['constans']['axis_count']['value']
+
+        self.button_clicked()
+
+    def right_clicked(self):
+        self.axis += 1
+        if self.axis > sim_settings['constans']['axis_count']['value']:
+            self.axis = 1
+
+        self.button_clicked()
+
+    def button_clicked(self):
+        self.edit_axis_signal.emit(self.axis)
+        self.label.setText(f"X<sub>{self.axis}</sub>")
--- src/gui/widgets/center_panel.py
+++ src/gui/widgets/center_panel.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import (QVBoxLayout, QGridLayout, QHBoxLayout, QLabel, QWidget)
 from src.config_loader import ConfigLoader
-from src.gui.widgets.base_components import GraphWidget
+from src.gui.widgets.base_components import GraphWidget, AxisSelectionWidget
 
 gui_settings = ConfigLoader.get_gui_settings()
 
@@ -11,22 +11,41 @@ class CenterPanel(QWidget):
 
         center_widget = QWidget()
         layout = QVBoxLayout(center_widget)
-
-        center_widget.setStyleSheet('border: 2px solid #b0b0b0;')
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
 
         graph_layout = QHBoxLayout()
         axis_layout = QHBoxLayout()
-        color_display_layout = QVBoxLayout()
+        color_display_layout = QHBoxLayout()
         button_layout = QGridLayout()
 
-        axis_displayed, self.graph_widgets = 1, []
+        axis_layout.setContentsMargins(0, 0, 0, 0)
+
+        axis_displayed, self.graph_widgets, self.axis_selection_widgets = 1, [], []
         for i in range(gui_settings['graph']['quantity_graph_widgets']):
+            graph_size = gui_settings['panels']['center_width'] / gui_settings['graph']['quantity_graph_widgets']
+
             graph_widget = GraphWidget(
-                gui_settings['panels']['center_width'] / gui_settings['graph']['quantity_graph_widgets'],
-                axis_displayed, axis_displayed+1)
+                graph_size, axis_displayed, axis_displayed+1
+            )
+
+            axis_selection_widget_1 = AxisSelectionWidget(axis_displayed, graph_size)
+            axis_selection_widget_2 = AxisSelectionWidget(axis_displayed+1, graph_size)
+
+            axis_selection_widget_1.edit_axis_signal.connect(graph_widget.edit_bottom_axis)
+            axis_selection_widget_2.edit_axis_signal.connect(graph_widget.edit_left_axis)
+
             axis_displayed += 2
+
             self.graph_widgets.append(graph_widget)
             graph_layout.addWidget(graph_widget)
+
+            self.axis_selection_widgets.append([
+                axis_selection_widget_1,
+                axis_selection_widget_2
+            ])
+            axis_layout.addWidget(axis_selection_widget_1)
+            axis_layout.addWidget(axis_selection_widget_2)
 
         layout.addLayout(graph_layout)
         layout.addLayout(axis_layout)
